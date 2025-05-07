002033c1f804ba112fc92d16138b64722a971b1b
danzxxhosting-tech <smptdanzxx@gmail.com>
2025-05-07 12:55:35 +0700

    Update images.js

--- api/images.js
+++ api/images.js
@@ -1,1 +1,10 @@
 
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const imagesPath = path.resolve('data', 'images.json');
+  const imagesData = fs.readFileSync(imagesPath, 'utf-8');
+  const images = JSON.parse(imagesData);
+  res.status(200).json(images);
+}
