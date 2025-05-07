91a6cb9a32ac0ac7d61def11817cdac181fc1f9c
danzxxhosting-tech <smptdanzxx@gmail.com>
2025-05-07 12:55:12 +0700

    Update stickers.js

--- api/stickers.js
+++ api/stickers.js
@@ -1,1 +1,10 @@
 
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const stickersPath = path.resolve('data', 'stickers.json');
+  const stickersData = fs.readFileSync(stickersPath, 'utf-8');
+  const stickers = JSON.parse(stickersData);
+  res.status(200).json(stickers);
+}
