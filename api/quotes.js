fd0bebf028daabbc64846f7eb6c2f651875b9aa3
danzxxhosting-tech <smptdanzxx@gmail.com>
2025-05-07 12:54:19 +0700

    Update quotes.js

--- api/quotes.js
+++ api/quotes.js
@@ -1,1 +1,10 @@
 
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const quotesPath = path.resolve('data', 'quotes.json');
+  const quotesData = fs.readFileSync(quotesPath, 'utf-8');
+  const quotes = JSON.parse(quotesData);
+  res.status(200).json(quotes);
+}
