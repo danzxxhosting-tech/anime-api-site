bf860ffd57e0ef11785d77cb89b01053d3e20c8a
danzxxhosting-tech <smptdanzxx@gmail.com>
2025-05-08 20:53:55 +0700

    Create naruto.js

---
+++ api/naruto.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const quotesPath = path.resolve('data', 'naruto.json');
+  const quotesData = fs.readFileSync(quotesPath, 'utf-8');
+  const quotes = JSON.parse(quotesData);
+  res.status(200).json(quotes);
+}
